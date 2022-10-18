12c991b66144c0613a25738e907122f923385f51
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-10-18 00:54:53 -0300

    feat: requisito 17 - adiciona uma query para criar uma nova collection chamada "resumoProdutos" que armazena o nome da franquia e o total de de produtos que ela possui, e outrea querry para exibir o nome da franquia e o total de produtos

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,1 +1,5 @@
-db.produtos.find();
+const totalProdutos = db.produtos.countDocuments({});
+
+db.resumoProdutos.insertOne({ franquia: "McDonalds", totalProdutos });
+
+db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
