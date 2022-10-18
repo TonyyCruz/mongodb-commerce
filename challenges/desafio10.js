0372ea8e32a0ad3895528bafca1ef18f6942ca61
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-10-17 21:52:29 -0300

    refactor: requisito 10 - retira o operador $all e faz as duas verificacoes juntas

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,2 @@
-db.produtos.find({ $and: [{ "valoresNutricionais.3.percentual": { $gte: 30 } },
-  { "valoresNutricionais.3.percentual": { $lte: 40 } }] },
+db.produtos.find({ "valoresNutricionais.3.percentual": { $gte: 30, $lte: 40 } },
   { nome: 1, _id: 0 });
