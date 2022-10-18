2f517832c76a34dd848f11b5f1a9bc1d9df3ba2c
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-10-18 00:33:35 -0300

    feat: requisito 16 - cria uma query para adicionar o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac e outra para retornar o nome de todos os documentos em que o campo ultimaModificacao existe

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,1 +1,3 @@
-db.produtos.find();
+db.produtos.updateMany({ nome: "Big Mac" }, { $set: { ultimaModificacao: new Date() } });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
